167c3e173f4d17b6da431df7a8f9926fbed80262
Kenan Scott <kenan@kenanscott.com>
2017-11-14 14:04:08 -0700

    add googleapis

--- write-sheet.js
+++ write-sheet.js
@@ -1,7 +1,9 @@
 /*jshint esversion: 6 */
 const AWSXRay = require('aws-xray-sdk-core');
 const AWS = AWSXRay.captureAWS(require('aws-sdk'));
+const google = require('googleapis');
+const sheets = google.sheets('v4');
 
 exports.handler = (event, context, callback) => {
-
+  
 };
