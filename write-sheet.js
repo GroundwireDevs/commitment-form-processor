d91e821ba0b31bb23f34ba7a59aebc2171815437
Kenan Scott <kenan@kenanscott.com>
2017-11-14 11:08:47 -0700

    Require google-spreadsheet

--- write-sheet.js
+++ write-sheet.js
@@ -1,6 +1,7 @@
 /*jshint esversion: 6 */
 const AWSXRay = require('aws-xray-sdk-core');
 const AWS = AWSXRay.captureAWS(require('aws-sdk'));
+const GoogleSpreadsheet = require('google-spreadsheet');
 
 const encrypted = process.env['private_key'];
 let decrypted;
