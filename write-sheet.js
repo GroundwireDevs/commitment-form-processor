bcb73df28dd717cb889b42097bf2179d4546537c
Kenan Scott <kenan@kenanscott.com>
2017-11-14 10:17:29 -0700

    Add write-sheet start and packages

--- write-sheet.js
+++ write-sheet.js
@@ -1,3 +1,50 @@
 /*jshint esversion: 6 */
 const AWSXRay = require('aws-xray-sdk-core');
 const AWS = AWSXRay.captureAWS(require('aws-sdk'));
+
+const encrypted = process.env['private_key'];
+let decrypted;
+
+// spreadsheet key is the long id in the sheets URL
+var doc = new GoogleSpreadsheet(process.env.SPREADSHEET_KEY);
+var sheet;
+
+function setAuth() {
+  return new Promise(function(resolve, reject) {
+
+  var creds = {
+    client_email: process.env.client_email,
+    private_key: decrypted
+  };
+
+  doc.useServiceAccountAuth(creds, function(err, data) {
+    if (err) reject(Error(err));
+    else resolve(data);
+  });
+
+  });
+}
+
+function processEvent(event, context, callback) {
+  setAuth.then()
+}
+
+exports.handler = (event, context, callback) => {
+  if (decrypted) {
+    processEvent(event, context, callback);
+  } else {
+    // Decrypt code should run once and variables stored outside of the function
+    // handler so that these are decrypted once per container
+    const kms = new AWS.KMS();
+    kms.decrypt({
+      CiphertextBlob: new Buffer(encrypted, 'base64')
+    }, (err, data) => {
+      if (err) {
+        console.log('Decrypt error:', err);
+        return callback(err);
+      }
+      decrypted = data.Plaintext.toString('ascii');
+      processEvent(event, context, callback);
+    });
+  }
+};
