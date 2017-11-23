8e48f732524ceb68a23c3a7ae93d54a6c8cc1b76
Kenan Scott <kenan@kenanscott.com>
2017-11-22 21:27:11 -0700

    Fix indentations

--- reply.js
+++ reply.js
@@ -5,36 +5,36 @@ const ses = new AWS.SES();
 const templateMapping = require('./template-map.json');
 
 function sendTemplatedEmail(to, template, source) {
-  return new Promise(function(resolve, reject) {
+	return new Promise(function(resolve, reject) {
 
-    const params = {
-      Destination: {
-        ToAddresses: [
-          to
-        ]
-      },
-      Source: source,
-      Template: template,
-      TemplateData: '{}'
-    };
+		const params = {
+			Destination: {
+				ToAddresses: [
+					to
+				]
+			},
+			Source: source,
+			Template: template,
+			TemplateData: '{}'
+		};
 
-    console.log(templateMapping);
-    console.log(params);
+		console.log(templateMapping);
+		console.log(params);
 
-    ses.sendTemplatedEmail(params, function(err, data) {
-      if (err) reject(Error(err)); // an error occurred
-      else resolve(data); // successful response
-    });
+		ses.sendTemplatedEmail(params, function(err, data) {
+			if (err) reject(Error(err)); // an error occurred
+			else resolve(data); // successful response
+		});
 
-  });
+	});
 }
 
 exports.handler = (event, context, callback) => {
-  const source = templateMapping[event.language].fromName + ' <' + templateMapping[event.language].fromAddress + '>';
-  sendTemplatedEmail(event.email, templateMapping[event.language][event.type], source).then(function(data) {
-    callback(null, data);
-  }).catch(function(err) {
-    console.error(err);
-    callback(err);
-  });
+	const source = templateMapping[event.language].fromName + ' <' + templateMapping[event.language].fromAddress + '>';
+	sendTemplatedEmail(event.email, templateMapping[event.language][event.type], source).then(function(data) {
+		callback(null, data);
+	}).catch(function(err) {
+		console.error(err);
+		callback(err);
+	});
 };
