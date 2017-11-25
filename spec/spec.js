fe91f0ac4e21c2750bb5ca30d169b27d90ee5469
Kenan Scott <kenan@kenanscott.com>
2017-11-25 13:18:13 -0700

    Start test suite

--- spec/spec.js
+++ spec/spec.js
@@ -1,13 +1,16 @@
-describe('Player', function() {
+describe('Reply function', function() {
 	const Reply = require('../reply.js');
-  const WriteSheet = require('../write-sheet.js');
+	const WriteSheet = require('../write-sheet.js');
+	const event = require('../spec/event.json');
+	const context = require('../spec/context.json');
 
-	it('should be able to play a Song', function() {
-		player.play(song);
-		expect(player.currentlyPlayingSong).toEqual(song);
-
-		//demonstrates use of custom matcher
-		expect(player).toBePlaying(song);
+	it('should be able to send a templated email.', function() {
+		process.env.AWS_XRAY_CONTEXT_MISSING = 'LOG_ERROR';
+		process.env.TESTING = true;
+		Reply.handler(event, context, function(err, data) {
+			expect(err).toEqual(null);
+			expect(data).not.toEqual(null);
+		});
 	});
 
 });
