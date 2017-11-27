23eb0182dfe758d57ee90608dc54a39c6b97bbed
Kenan Scott <kenan@kenanscott.com>
2017-11-27 12:32:26 -0700

    Remove env var settings

--- spec/spec.js
+++ spec/spec.js
@@ -5,8 +5,6 @@ describe('Reply function', function() {
 	const context = require('../spec/context.json');
 
 	it('should be able to send a templated email.', function() {
-		process.env.AWS_XRAY_CONTEXT_MISSING = 'LOG_ERROR';
-		process.env.TESTING = true;
 		Reply.handler(event, context, function(err, data) {
 			expect(err).toEqual(null);
 			expect(data).not.toEqual(null);
