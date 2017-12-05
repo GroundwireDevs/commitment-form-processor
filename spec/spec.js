bf32972bf13d27e2f0c8150748319febcad5779a
Kenan Scott <kenan@kenanscott.com>
2017-12-05 16:01:56 -0700

    Add to spec

--- spec/spec.js
+++ spec/spec.js
@@ -2,13 +2,33 @@ const LambdaTester = require( 'lambda-tester' );
 
 const myHandler = require( '../src/reply' ).handler;
 
-describe( 'handler', function() {
+describe( 'Reply function', function() {
 
-    it( 'test success', function() {
+	it( 'should send an email using a valid event, with age.', function() {
 
-        return LambdaTester( myHandler )
-            .event( {"language": "en","type":"salvation","firstName":"Kenan","lastName":"Scott","email":"kenans@groundwire.net","commitment":"no","age":"5"}
- )
-            .expectResult();
-    });
+		// Valid data with age
+		return LambdaTester( myHandler )
+			.event( {'language': 'en','type':'salvation','firstName':'Kenan','lastName':'Scott','email':'kenans@groundwire.net','commitment':'no','age':'5'})
+			.expectResult();
+
+	});
+
+	it( 'should send an email using a valid event, without age.', function() {
+
+		// Valid data with age
+		return LambdaTester( myHandler )
+			.event( {'language': 'en','type':'salvation','firstName':'Kenan','lastName':'Scott','email':'kenans@groundwire.net','commitment':'no'})
+			.expectResult();
+
+	});
+
+  it( 'shouldn\'t send an email using an invalid event key.', function() {
+
+		// Valid data with age
+		return LambdaTester( myHandler )
+			.event( {'language': 'en','type':'salvation','firstName':'Kenan','lastName':'Scott','e-mail':'kenans@groundwire.net','commitment':'no'})
+			.expectResult().toThrow();
+
+	});
+
 });
