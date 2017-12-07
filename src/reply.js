dcd5a7b9df1fd2df4f8fc0ca4147ef581a095ca8
Kenan Scott <kenan@kenanscott.com>
2017-12-07 13:21:25 -0700

    Set region to us-west-2 if in testing mode

--- src/reply.js
+++ src/reply.js
@@ -1,11 +1,12 @@
 'use strict';
 const AWSXRay = require('aws-xray-sdk-core');
 const AWS = AWSXRay.captureAWS(require('aws-sdk'));
-const ses = new AWS.SES({region: 'us-west-2'});
 const templateMapping = require('./../template-map.json');
 
 function sendTemplatedEmail(to, template, source) {
 	return new Promise(function(resolve, reject) {
+
+		const ses = new AWS.SES({region: process.env.AWS_REGION});
 
 		const params = {
 			Destination: {
@@ -30,7 +31,10 @@ function sendTemplatedEmail(to, template, source) {
 }
 
 exports.handler = (event, context, callback) => {
-	if (event.testing === true) process.env._X_AMZN_TRACE_ID = 'Root=1-5759e988-bd862e3fe1be46a994272793;Parent=53995c3f42cd8ad8;Sampled=1';
+	if (event.testing === true) {
+		process.env._X_AMZN_TRACE_ID = 'Root=1-5759e988-bd862e3fe1be46a994272793;Parent=53995c3f42cd8ad8;Sampled=1';
+		process.env.AWS_REGION = 'us-west-2';
+	}
 	const source = templateMapping[event.language].fromName + ' <' + templateMapping[event.language].fromAddress + '>';
 	sendTemplatedEmail(event.email, templateMapping[event.language][event.type], source).then(function(data) {
 		callback(null, data);
