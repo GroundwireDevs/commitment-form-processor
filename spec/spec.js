11b8c4a709683787231cf30476debe2e5e08cdab
Kenan Scott <kenan@kenanscott.com>
2017-12-07 14:10:34 -0700

    Remove now unused test

--- spec/spec.js
+++ spec/spec.js
@@ -1,14 +1,14 @@
-const LambdaTester = require( 'lambda-tester' );
+const LambdaTester = require('lambda-tester');
 
-const myHandler = require( '../src/reply' ).handler;
+const myHandler = require('../src/reply').handler;
 
-describe( 'Reply function', function() {
+describe('Reply function', function() {
 
 	it( 'should send an email using a valid event, with age.', function() {
 
 		// Valid data with age
-		return LambdaTester( myHandler )
-			.event( {'language': 'en','type':'salvation','firstName':'Kenan','lastName':'Scott','email':'kenans@groundwire.net','commitment':'no','age':'5'})
+		return LambdaTester(myHandler)
+			.event({'testing': true, 'language': 'en','type':'salvation','firstName':'Kenan','lastName':'Scott','email':'kenans@groundwire.net','commitment':'no','age':'5'})
 			.expectResult();
 
 	});
@@ -16,17 +16,26 @@ describe( 'Reply function', function() {
 	it( 'should send an email using a valid event, without age.', function() {
 
 		// Valid data with age
-		return LambdaTester( myHandler )
-			.event( {'language': 'en','type':'salvation','firstName':'Kenan','lastName':'Scott','email':'kenans@groundwire.net','commitment':'no'})
+		return LambdaTester(myHandler)
+			.event({'testing': true, 'language': 'en','type':'salvation','firstName':'Kenan','lastName':'Scott','email':'kenans@groundwire.net','commitment':'no'})
 			.expectResult();
 
 	});
 
-  it( 'shouldn\'t send an email using an invalid event key.', function() {
+	it( 'shouldn\'t send an email using an invalid event key for email.', function() {
 
 		// Valid data with age
-		return LambdaTester( myHandler )
-			.event( {'language': 'en','type':'salvation','firstName':'Kenan','lastName':'Scott','e-mail':'kenans@groundwire.net','commitment':'no'})
+		return LambdaTester(myHandler)
+			.event({'testing': true, 'language': 'en','type':'salvation','firstName':'Kenan','lastName':'Scott','e-mail':'kenans@groundwire.net','commitment':'no'})
+			.expectError();
+
+	});
+
+	it( 'shouldn\'t send an email if no input is provided.', function() {
+
+		// Valid data with age
+		return LambdaTester(myHandler)
+			.event({'testing': true} )
 			.expectError();
 
 	});
